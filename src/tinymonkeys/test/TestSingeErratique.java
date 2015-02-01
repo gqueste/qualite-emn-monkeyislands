1d80491603151cc0e14015db74ebebddde865626
gqueste <gabriel.queste@gmail.com>
2015-02-01 15:33:17 +0100

    Fin des tests mockés pour SingeErratique

--- src/tinymonkeys/test/TestSingeErratique.java
+++ src/tinymonkeys/test/TestSingeErratique.java
@@ -12,7 +12,7 @@ import tinymonkeys.modele.Pirate;
 import tinymonkeys.modele.SingeErratique;
 
 /**
- * Classe de test.
+ * Classe de test pour la classe SingeErratique en version mockée.
  * @author Gabriel Queste
  *
  */
@@ -30,20 +30,15 @@ public class TestSingeErratique extends TestCase{
 	
 
 	@Before
+	/**
+	 * Mocke l'ile et initialise le singe.
+	 */
 	public void setUp() throws Exception {
 		this.ile = EasyMock.createMock(Ile.class);
 		this.singe = new SingeErratique(10, 10, ile);
 	}
 
-	//déplacement de chaque côté
-	//blocage
-	//équiprobabilité
-	
-	//est ce que le singe se déplace bien
-	//1 seule posibilité à droite, à gauche,...
-	
 	//versions non bouchonnée ensuite pour voir s'il n'y a pas des erreurs plus tard
-	
 	
 	
 	//--- Tests des déplacements permis
@@ -64,7 +59,8 @@ public class TestSingeErratique extends TestCase{
 	
 	@Test
 	/**
-	 * 
+	 * test si le déplacement est permis alors qu'il y a un singe,
+	 * mais il ne bloque pas
 	 */
 	public void testDeplacementSingeOK_OKSingesExistant() {
 		int positionX = 5;
@@ -82,6 +78,9 @@ public class TestSingeErratique extends TestCase{
 	}
 	
 	@Test
+	/**
+	 * test si le déplacement n'est pas permis à cause d'un singe
+	 */
 	public void testDeplacementSingeOK_KOSingeBloquant() {
 		int positionX = 5;
 		int positionY = 5;
@@ -98,6 +97,9 @@ public class TestSingeErratique extends TestCase{
 	}
 	
 	@Test
+	/**
+	 * test si le déplacement n'est pas permis parce qu'il y a de l'eau
+	 */
 	public void testDeplacementSingeOK_KOEau() {
 		int positionX = 5;
 		int positionY = 5;
@@ -114,8 +116,10 @@ public class TestSingeErratique extends TestCase{
 	}
 	
 	//--- Tests des déplacements effectués
-	
 	@Test
+	/**
+	 * test des déplacements effectués : deplacement permis
+	 */
 	public void testDeplacementFait_OKDeplacementPermis() {
 		int positionX = 5;
 		int positionY = 5;
@@ -130,11 +134,14 @@ public class TestSingeErratique extends TestCase{
 		
 		assertTrue("deplacement non effectué alors que permis",
 				this.singe.deplacementFait(positionX, positionY));
-		assertTrue("X n a pas changé", this.singe.getX() == positionX);
-		assertTrue("Y n a pas changé", this.singe.getY() == positionY);
+		assertEquals("X n a pas changé", this.singe.getX(), positionX);
+		assertEquals("Y n a pas changé", this.singe.getY(), positionY);
 	}
 	
 	@Test
+	/**
+	 * test des déplacements effectués : deplacement non permis
+	 */
 	public void testDeplacementFait_KODeplacementNonPermis() {
 		int positionX = 5;
 		int positionY = 5;
@@ -152,6 +159,7 @@ public class TestSingeErratique extends TestCase{
 		assertFalse("Y changé", this.singe.getY() == positionY);
 	}
 	
+	//--- Tests des interactions avec le pirate
 	@Test
 	/**
 	 * Tue le pirate s'il se trouve à l'endroit du déplacement
@@ -196,7 +204,11 @@ public class TestSingeErratique extends TestCase{
 		singe.gereDeplacementSingePirate();
 	}
 	
+	//--- Tests de l'aléatoire
 	@Test
+	/**
+	 * Tests de l'aléatoire : Aucune position possible
+	 */
 	public void testDeplacerSinge_Blocage() {
 		int positionX = 10;
 		int positionY = 10;
@@ -216,7 +228,202 @@ public class TestSingeErratique extends TestCase{
 		EasyMock.replay(pirate);
 		singe.deplacerSinge();
 		
-		assertTrue("X changé", this.singe.getX() == positionX);
-		assertTrue("Y a changé", this.singe.getY() == positionY);
+		assertEquals("X changé", this.singe.getX(), positionX);
+		assertEquals("Y a changé", this.singe.getY(), positionY);
+	}
+	
+	@Test
+	/**
+	 * Tests de l'aléatoire : Seule position possible -> North
+	 */
+	public void testDeplacerSinge_South() {
+		int positionX = 10;
+		int positionY = 10;
+		
+		Pirate pirate = EasyMock.createMock(Pirate.class);
+		
+		EasyMock.expect(this.ile.estPositionSurTerre(positionX + 1, positionY)).andReturn(false).anyTimes();
+		EasyMock.expect(this.ile.estPositionSurTerre(positionX - 1, positionY)).andReturn(false).anyTimes();
+		EasyMock.expect(this.ile.estPositionSurTerre(positionX, positionY + 1)).andReturn(false).anyTimes();
+		EasyMock.expect(this.ile.estPositionSurTerre(positionX, positionY - 1)).andReturn(true).anyTimes();
+		
+		BandeDeSingesErratiques bande = new BandeDeSingesErratiques(this.ile);
+		SingeErratique s1 = new SingeErratique(5, 5, this.ile);
+		bande.ajoutSinge(s1);
+		
+		EasyMock.expect(this.ile.getSingesErratiques()).andReturn(bande);
+		
+		EasyMock.expect(this.ile.getPirate()).andReturn(pirate);
+		
+		EasyMock.expect(pirate.getX()).andReturn(3);
+		
+		EasyMock.replay(this.ile);
+		EasyMock.replay(pirate);
+		singe.deplacerSinge();
+		
+		assertEquals("X changé", this.singe.getX(), positionX);
+		assertEquals("Y incorrect", this.singe.getY(), positionY - 1);
+	}
+	
+	@Test
+	/**
+	 * Tests de l'aléatoire : Seule position possible -> South
+	 */
+	public void testDeplacerSinge_North() {
+		int positionX = 10;
+		int positionY = 10;
+		
+		Pirate pirate = EasyMock.createMock(Pirate.class);
+		
+		EasyMock.expect(this.ile.estPositionSurTerre(positionX + 1, positionY)).andReturn(false).anyTimes();
+		EasyMock.expect(this.ile.estPositionSurTerre(positionX - 1, positionY)).andReturn(false).anyTimes();
+		EasyMock.expect(this.ile.estPositionSurTerre(positionX, positionY + 1)).andReturn(true).anyTimes();
+		EasyMock.expect(this.ile.estPositionSurTerre(positionX, positionY - 1)).andReturn(false).anyTimes();
+		
+		BandeDeSingesErratiques bande = new BandeDeSingesErratiques(this.ile);
+		SingeErratique s1 = new SingeErratique(5, 5, this.ile);
+		bande.ajoutSinge(s1);
+		
+		EasyMock.expect(this.ile.getSingesErratiques()).andReturn(bande);
+		
+		EasyMock.expect(this.ile.getPirate()).andReturn(pirate);
+		
+		EasyMock.expect(pirate.getX()).andReturn(3);
+		
+		EasyMock.replay(this.ile);
+		EasyMock.replay(pirate);
+		singe.deplacerSinge();
+		
+		assertEquals("X changé", this.singe.getX(), positionX);
+		assertEquals("Y incorrect", this.singe.getY(), positionY + 1);
+	}
+	
+	@Test
+	/**
+	 * Tests de l'aléatoire : Seule position possible -> West
+	 */
+	public void testDeplacerSinge_West() {
+		int positionX = 10;
+		int positionY = 10;
+		
+		Pirate pirate = EasyMock.createMock(Pirate.class);
+		
+		EasyMock.expect(this.ile.estPositionSurTerre(positionX + 1, positionY)).andReturn(false).anyTimes();
+		EasyMock.expect(this.ile.estPositionSurTerre(positionX - 1, positionY)).andReturn(true).anyTimes();
+		EasyMock.expect(this.ile.estPositionSurTerre(positionX, positionY + 1)).andReturn(false).anyTimes();
+		EasyMock.expect(this.ile.estPositionSurTerre(positionX, positionY - 1)).andReturn(false).anyTimes();
+		
+		BandeDeSingesErratiques bande = new BandeDeSingesErratiques(this.ile);
+		SingeErratique s1 = new SingeErratique(5, 5, this.ile);
+		bande.ajoutSinge(s1);
+		
+		EasyMock.expect(this.ile.getSingesErratiques()).andReturn(bande);
+		
+		EasyMock.expect(this.ile.getPirate()).andReturn(pirate);
+		
+		EasyMock.expect(pirate.getX()).andReturn(3);
+		
+		EasyMock.replay(this.ile);
+		EasyMock.replay(pirate);
+		singe.deplacerSinge();
+		
+		assertEquals("X incorrect", this.singe.getX(), positionX - 1);
+		assertEquals("Y incorrect", this.singe.getY(), positionY);
+	}
+	
+	@Test
+	/**
+	 * Tests de l'aléatoire : Seule position possible -> East
+	 */
+	public void testDeplacerSinge_East() {
+		int positionX = 10;
+		int positionY = 10;
+		
+		Pirate pirate = EasyMock.createMock(Pirate.class);
+		
+		EasyMock.expect(this.ile.estPositionSurTerre(positionX + 1, positionY)).andReturn(true).anyTimes();
+		EasyMock.expect(this.ile.estPositionSurTerre(positionX - 1, positionY)).andReturn(false).anyTimes();
+		EasyMock.expect(this.ile.estPositionSurTerre(positionX, positionY + 1)).andReturn(false).anyTimes();
+		EasyMock.expect(this.ile.estPositionSurTerre(positionX, positionY - 1)).andReturn(false).anyTimes();
+		
+		BandeDeSingesErratiques bande = new BandeDeSingesErratiques(this.ile);
+		SingeErratique s1 = new SingeErratique(5, 5, this.ile);
+		bande.ajoutSinge(s1);
+		
+		EasyMock.expect(this.ile.getSingesErratiques()).andReturn(bande);
+		
+		EasyMock.expect(this.ile.getPirate()).andReturn(pirate);
+		
+		EasyMock.expect(pirate.getX()).andReturn(3);
+		
+		EasyMock.replay(this.ile);
+		EasyMock.replay(pirate);
+		singe.deplacerSinge();
+		
+		assertEquals("X incorrect", this.singe.getX(), positionX + 1);
+		assertEquals("Y incorrect", this.singe.getY(), positionY);
+	}
+	
+	@Test
+	/**
+	 * Tests de l'aléatoire : Equiprobabilité des possibilités
+	 */
+	public void testDeplacerSinge_Equiprobabilite() {
+		int deplacement_North = 0;
+		int deplacement_South = 0;
+		int deplacement_West = 0;
+		int deplacement_East = 0;
+		
+		int positionX = 10;
+		int positionY = 10;
+		
+		int nbEssais = 10000;
+		double nbAttendu = nbEssais / 4;
+		double margeSup = nbAttendu + 200;	
+		double margeInf = nbAttendu - 200;	
+		
+		Pirate pirate = EasyMock.createMock(Pirate.class);
+		
+		EasyMock.expect(this.ile.estPositionSurTerre(positionX + 1, positionY)).andReturn(true).anyTimes();
+		EasyMock.expect(this.ile.estPositionSurTerre(positionX - 1, positionY)).andReturn(true).anyTimes();
+		EasyMock.expect(this.ile.estPositionSurTerre(positionX, positionY + 1)).andReturn(true).anyTimes();
+		EasyMock.expect(this.ile.estPositionSurTerre(positionX, positionY - 1)).andReturn(true).anyTimes();
+		
+		BandeDeSingesErratiques bande = new BandeDeSingesErratiques(this.ile);
+		SingeErratique s1 = new SingeErratique(5, 5, this.ile);
+		bande.ajoutSinge(s1);
+		
+		EasyMock.expect(this.ile.getSingesErratiques()).andReturn(bande).anyTimes();;
+		
+		EasyMock.expect(this.ile.getPirate()).andReturn(pirate).anyTimes();;
+		
+		EasyMock.expect(pirate.getX()).andReturn(3).anyTimes();;
+		
+		EasyMock.replay(this.ile);
+		EasyMock.replay(pirate);
+		
+		for (int i = 0; i < nbEssais; i++) {
+			this.singe.setPosition(positionX, positionY);
+			
+			singe.deplacerSinge();
+			
+			if (singe.getX() == positionX && singe.getY() == positionY + 1) {
+				deplacement_North ++;
+			}
+			else if (singe.getX() == positionX && singe.getY() == positionY - 1) {
+				deplacement_South ++;
+			}
+			else if (singe.getX() == positionX + 1 && singe.getY() == positionY) {
+				deplacement_East ++;
+			}
+			else if (singe.getX() == positionX - 1 && singe.getY() == positionY) {
+				deplacement_West ++;
+			}
+		}
+		
+		assertTrue("North pas équiprobable", deplacement_North >= margeInf && deplacement_North <= margeSup);
+		assertTrue("South pas équiprobable", deplacement_South >= margeInf && deplacement_South <= margeSup);
+		assertTrue("East pas équiprobable", deplacement_East >= margeInf && deplacement_East <= margeSup);
+		assertTrue("West pas équiprobable", deplacement_West >= margeInf && deplacement_West <= margeSup);
 	}
 }
